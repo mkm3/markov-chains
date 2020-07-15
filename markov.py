81f2f557d1d5f4be061e745cf098af854115a0ad
Michelle Macaraeg <macaraegm@gmail.com>
2020-07-15 00:48:09 +0000

    Worked on make_chains() & make_text()

--- markov.py
+++ markov.py
@@ -1,5 +1,6 @@
 """Generate Markov text from text files."""
 
+import sys
 from random import choice
 
 
@@ -9,10 +10,13 @@ def open_and_read_file(file_path):
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
+    contents = open(file_path)
+    text = contents.read()
+    contents.close()
 
-    # your code goes here
+    return text
 
-    return "Contents of your file as one long string"
+# open_and_read_file('green-eggs.txt')
 
 
 def make_chains(text_string):
@@ -39,25 +43,58 @@ def make_chains(text_string):
         >>> chains[('there','juanita')]
         [None]
     """
+    
+    # contents = open_and_read_file('green-eggs.txt')
 
     chains = {}
 
-    # your code goes here
+    words = text_string.split()
+
+    words.append(None)
+
+    # for i in range(len(words) - 1):
+    #     print(words[i], words[i + 1])
+
+    for idx in range(len(words) - 2):
+        
+        key = (words[idx], words[idx + 1])
+        value = words[idx + 2]
+
+    # for word in range(len(words) - 1):
+    #     chains[words[word], words[word + 1]] = chains[words[word], words[word + 2]]
+
+        if key not in chains:
+            chains[key] = []
+
+        chains[key].append(value)
+
+
+    # for item, value in chains:
+    #     print(f'{item}:{value}')
 
     return chains
+
+# print(make_chains('green-eggs.txt'))
 
 
 def make_text(chains):
     """Return text from chains."""
 
-    words = []
+    key = choice(list(chains.keys()))
+    words = [key[0], key[1]]
+    word = choice(chains[key])
 
-    # your code goes here
+    while word is not None:
+        key = (key[1], word)
+        words.append(word)
+        word = choice(chains[key])
 
+        # your code goes here
     return " ".join(words)
 
 
-input_path = "green-eggs.txt"
+
+input_path = sys.argv[0]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
